3b89d2962fd9c85b981e9d1f4e99df9daff9e678
Oliver Plunkett <otplunkett@gmail.com>
2018-12-21 17:52:51 -0400

    qa

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,8 +3,7 @@ require("dotenv").config()
 module.exports = {
   PORT: process.env.PORT || 3002,
   MONGODB_URI:
-    "mongodb://heroku_mw9ffrnp:gu2039fjju324omeufcnpul9f4@ds233531.mlab.com:33531/heroku_mw9ffrnp" /*process.env.MONGODB_URI*/ ||
-    "mongodb://localhost:27017/invisible-college",
+    process.env.MONGODB_URI || "mongodb://localhost:27017/invisible-college",
   REDIS_URL: process.env.REDISTOGO_URL,
   AWS_IDENTITY_POOL_ID: process.env.AWS_IDENTITY_POOL_ID,
   AWS_REGION: process.env.AWS_REGION,
